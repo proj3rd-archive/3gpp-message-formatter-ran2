9b380700869c5ae44548de58a406bf7b2f6fb31b
Seokseong Jeon <sjeon87@gmail.com>
2018-10-23 14:18:39 +0900

    Output ASN.1 text format

--- formatter.js
+++ formatter.js
@@ -7,11 +7,13 @@ var getUniqueMessageIE = parser.getUniqueMessageIE;
 var xlsx = require('@gsongsong/xlsx');
 var libExpand = require('./lib/expand');
 var libFormatXlsx = require('./lib/formatXlsx');
+var libFormatTxt = require('./lib/formatTxt');
 
 var expand = libExpand.expand;
 var expandAll = libExpand.expandAll;
 var toWorksheet = libFormatXlsx.toWorksheet;
 var toWorkbook = libFormatXlsx.toWorkbook;
+var formatTxt = libFormatTxt.formatTxt;
 
 exports.expand = expand;
 exports.expandAll =  expandAll;
@@ -20,22 +22,33 @@ exports.toWorkbook = toWorkbook;
 exports.format = format;
 exports.formatAll = formatAll;
 
-function format(messageIEname, asn1Json, raw = false) {
-    let worksheets = [];
-    let styles = [];
+function format(messageIEname, asn1Json, raw = false, format = 'xlsx') {
     if (messageIEname == '__all') {
-        let messageIEs = expandAll(asn1Json, raw);
-        formatAll(messageIEs, worksheets, styles);
+        if (format == 'xlsx') {
+            let messageIEs = expandAll(asn1Json, raw);
+            let worksheets = [];
+            let styles = [];
+            formatAll(messageIEs, worksheets, styles);
+            return toWorkbook(worksheets, styles);
+        } else if (format == 'txt') {
+            // TODO later...
+        }
     } else {
         let messageIE = getUniqueMessageIE(messageIEname, asn1Json);
         messageIEHelper(messageIE, messageIEname);
         console.log(`Formatting ${messageIE['module']}/${messageIEname}...`);
         let depthMax = expand(messageIE, asn1Json, 0, raw);
-        let worksheetWithStyle = toWorksheet(messageIEname, messageIE, depthMax);
-        worksheets.push(worksheetWithStyle['worksheet']);
-        styles.push(worksheetWithStyle['style']);
+        if (format == 'xlsx') {
+            let worksheets = [];
+            let styles = [];
+            let worksheetWithStyle = toWorksheet(messageIEname, messageIE, depthMax);
+            worksheets.push(worksheetWithStyle['worksheet']);
+            styles.push(worksheetWithStyle['style']);
+            return toWorkbook(worksheets, styles);
+        } else if (format == 'txt') {
+            return formatTxt(messageIEname, messageIE);
+        }
     }
-    return toWorkbook(worksheets, styles);
 }
 
 function formatAll(messageIEs, worksheets, styles) {
@@ -64,14 +77,20 @@ if (require.main == module) {
     let argParser = new ArgumentParser({addHelp: true, debug: true});
     argParser.addArgument('specFile', {help: 'Sepcification file name'});
     argParser.addArgument('messageIEname', {help: 'Message or IE name'});
-    argParser.addArgument(['-r', '--raw'], {help: 'Do not expand sub IEs',
+    argParser.addArgument(['-r', '--raw'], {help: 'Do not expand sub IEs. txt output format forces raw',
                                             action: 'storeTrue'});
+    argParser.addArgument(['-f', '--format'], {defaultValue: 'xlsx',
+                                            help: 'Output format. [xlsx]/txt'});
     let args = {};
     try {
         args = argParser.parseArgs();
     } catch (e) {
         argParser.printHelp();
         process.exit();
+    }
+    // force raw
+    if (args.format == 'txt') {
+        args.raw = true;
     }
     let inputFile = path.parse(args.specFile);
     let input = extract(fs.readFileSync(path.resolve(process.cwd(),
@@ -89,8 +108,13 @@ if (require.main == module) {
     if (args.raw) {
         outputFile += '-raw';
     }
-    outputFile += '.xlsx';
-    xlsx.writeFile(format(messageIEname, asn1Json, args.raw), outputFile);
+    outputFile += `.${args.format}`;
+    let formatted = format(messageIEname, asn1Json, args.raw, args.format);
+    if (args.format == 'xlsx') {
+        xlsx.writeFile(formatted, outputFile);
+    } else if (args.format == 'txt') {
+        fs.writeFileSync(outputFile, formatted);
+    }
 }
 
 function logJson(json) {
